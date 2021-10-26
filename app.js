d3371e7b165c4042eab48dc24421b58c1fed94b8
HamidullahMuslih <hamidullahmuslih1@gmail.com>
2021-10-26 11:20:10 -0700

    added docker compose file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 function onRequest(request, response) {
     response.writeHead(200, {'Content-Type':'text/plain'});
-    response.write('Hello the test is fine');
+    response.write('Hello the node app is working fine');
     response.end();
 
 }
